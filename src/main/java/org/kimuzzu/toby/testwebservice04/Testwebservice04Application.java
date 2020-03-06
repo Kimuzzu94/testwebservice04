8139101b605e2d0c0f1500db464583e527afdac3
kimuzzu94 <kimuzzu@gmail.com>
2020-03-06 14:17:05 +0900

    Initial commit

---
+++ src/main/java/org/kimuzzu/toby/testwebservice04/Testwebservice04Application.java
@@ -0,0 +1,13 @@
+package org.kimuzzu.toby.testwebservice04;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Testwebservice04Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Testwebservice04Application.class, args);
+    }
+
+}
---
+++ src/test/java/org/kimuzzu/toby/testwebservice04/Testwebservice04ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.kimuzzu.toby.testwebservice04;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Testwebservice04ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
